b02364ec3eb24f8d63c0cc5a3eb3dc5a00971a17
Ben McLoughlin <benmcl@shaw.ca>
2020-11-16 12:59:56 -0800

    fixed donut chart

--- src/view/charts/createChartFunctions/createBarChart.js
+++ src/view/charts/createChartFunctions/createBarChart.js
@@ -5,7 +5,9 @@ import * as tooltips from "view/charts/tooltips/barTooltip"
 import { buildHtml } from "view/charts/tooltips/tooltip"
 import _ from "lodash"
 
+
 export const drawBarChart = (colors, className, data, dataObject, height, state, width) => {
+                       
   const { selectedId } = state.ui_reducer
   const stream = state.stream_reducer[selectedId]
 
@@ -55,8 +57,8 @@ export const drawBarChart = (colors, className, data, dataObject, height, state,
 
     const series = stack(data)
 
-    const y = d3.scaleLinear().range([graphHeight, 0]).domain([min, max])
-    const x = d3
+    const yScale = d3.scaleLinear().range([graphHeight, 0]).domain([min, max])
+    const xScale = d3
       .scaleBand()
       .range([0, graphWidth])
       .paddingInner(0.2)
@@ -67,85 +69,67 @@ export const drawBarChart = (colors, className, data, dataObject, height, state,
 
     rects.exit().remove()
 
-    const tooltip = d3.select(`.${className}`).append("div").attr("class", `${className}tooltip`).style("opacity", 0).style("position", "absolute").style("top", "-100rem").style("right", "30rem")
+    const tooltip = d3
+      .select(`.${className}`)
+      .append("div")
+      .attr("class", `${className}tooltip`)
+      .style("opacity", 0)
+      .style("position", "absolute")
+      .style("top", "-100rem")
+      .style("right", "30rem")
 
-    const rx = 12
-    const ry = 4
- 
-    
     rects
       .enter()
       .append("g")
-      .attr("fill", "steelblue")
+      .attr("fill", (d, i) => (colors[d.key] ? colors[d.key] : "#5E9090"))
       .attr("class", (d, i) => d.key)
-      .selectAll("path")
-      .data(data)
-      .enter()
-      .append("path")
+      .selectAll("rect")
       .data(d => d)
-      .attr("d", d => roundedRect(x(d.name), y(d.value), x.bandwidth(), y(0) - y(d.value), [10, 0, 0, 0]))
-      // .attr("d", d => {
-      //   console.log(d)
-      //   return ` 
-      //   M${x(d.data.year)},${y(d[1]) + ry}
-      //   a${rx},${ry} 0 0 1 ${rx},${-ry}
-      //   h${x.bandwidth() - 2 * rx}
-      //   a${rx},${ry} 0 0 1 ${rx},${ry}
-      //   v${height - y(d[0]) - y(d[1]) - ry}
-      //   h${-x.bandwidth()}Z
-      // `
-      // })
-
-      // .append("rect")
-      // .attr("x", d => x(d.data.year))
-      // .attr("borderRadius", "5px")
-      // .attr("class", (d, i, n) => {
-      //   const name = n[0].parentNode.className.animVal
-      //   return `${name}`
-      // })
-      // .attr("opacity", (d, i, n) => {
-      //   const name = n[0].parentNode.className.animVal
-      //   return streamName === name && d.data.year >= periodStart && d.data.year < periodEnd ? 0.7 : 1
-      // })
-      // .attr("width", x.bandwidth())
-      // .on("click", (d, i, n) => {
-      //   const name = n[0].parentNode.className.animVal
-      //   const id = Object.values(state.stream_reducer).filter(d => d.name === name)[0]["id"]
-      //   set("selectedId", "ui_reducer", id)
-      // })
-      // .attr("y", d => y(d[1]))
-      // .attr("height", d => (y(d[0]) > 0 ? y(d[0]) - y(d[1]) : 0))
-      // .style("cursor", "pointer")
-      // .on("mouseover", (d, i, n) => {
-      //   const name = n[0].parentNode.className.animVal
-      //   const color = colors[name]
-      //   tooltip.html(() => buildHtml(className, color, d, dataObject, n, state))
-      //   tooltip.transition().duration(200).style("opacity", 1).style("pointer-events", "none")
-      // })
-      // .on("mouseout", (d, i, n) => tooltip.transition().duration(1500).style("opacity", 0))
-      // .on("mousemove", () => {
-      //   tooltip
-      //     .style("top", d3.event.layerY - 20 + "px") // always 10px below the cursor
-      //     .style("left", d3.event.layerX + 30 + "px") // always 10px to the right of the mouse
-      // })
-
-    // .transition(t)
-    // .attrTween("width", d => widthTween(d, x))
-    // .attr("y", d => y(d[1]))
-    // .attr("height", d => (y(d[0]) > 0 ? y(d[0]) - y(d[1]) : 0))
+      .enter()
+      .append("rect")
+      .attr("x", d => xScale(d.data.year))
+      .attr("class", (d, i, n) => {
+        const name = n[0].parentNode.className.animVal
+        return `${name}`
+      })
+      .attr("opacity", (d, i, n) => {
+        const name = n[0].parentNode.className.animVal
+        return streamName === name && d.data.year >= periodStart && d.data.year < periodEnd ? 0.7 : 1
+      })
+      .attr("width", xScale.bandwidth())
+      .on("click", (d, i, n) => {
+        const name = n[0].parentNode.className.animVal
+        const id = Object.values(state.stream_reducer).filter(d => d.name === name)[0]["id"]
+        set("selectedId", "ui_reducer", id)
+      })
+      .attr("y", d => yScale(d[1]))
+      .attr("height", d => (yScale(d[0]) > 0 ? yScale(d[0]) - yScale(d[1]) : 0))
+      .style("cursor", "pointer")
+      .on("mouseover", (d, i, n) => {
+        const name = n[0].parentNode.className.animVal
+        const color = colors[name]
+        tooltip.html(() => buildHtml(className, color, d, dataObject, n, state))
+        tooltip.transition().duration(200).style("opacity", 1).style("pointer-events", "none")
+      })
+      .on("mouseout", (d, i, n) => tooltip.transition().duration(1500).style("opacity", 0))
+      .on("mousemove", () => {
+        tooltip
+          .style("top", d3.event.layerY - 20 + "px") // always 10px below the cursor
+          .style("left", d3.event.layerX + 30 + "px") // always 10px to the right of the mouse
+      })
 
     var ticks = [2020, 2040, 2060]
     var tickLabels = ["2020", "2040", "2060"]
 
     const xAxis = d3
-      .axisBottom(x)
+      .axisBottom(xScale)
       .tickValues(ticks)
       .tickFormat(function (d, i) {
         return tickLabels[i]
       })
 
     const yAxis = d3
-      .axisLeft(y)
+      .axisLeft(yScale)
       .ticks("2")
       .tickFormat(d => `${d / 1000}k`)
 
@@ -156,11 +140,4 @@ export const drawBarChart = (colors, className, data, dataObject, height, state,
   }
 
   update(data)
-}
-
-const widthTween = (d, x) => {
-  let i = d3.interpolate(0, x.bandwidth())
-  return function (t) {
-    return i(t)
-  }
 }
